8f3c7c280601cd2feadc52c9ac62dead3bb74287
AlfadliRPutra <alfadlipoetra@gmail.com>
2024-02-23 14:05:12 +0700

    add data

--- ecommerce.py
+++ ecommerce.py
@@ -16,7 +16,6 @@ def data_analysis(df):
     total_sales_all_payments = df['payment_value'].sum()
     total_transactions_all_payments = len(df)
     aov_all_payments = total_sales_all_payments / total_transactions_all_payments
-    st.write(f"Nilai pesanan rata-rata (AOV) untuk semua pembayaran adalah: {aov_all_payments}")
 
     # Pertanyaan 2: AOV berdasarkan metode pembayaran
     payment_stats = df.groupby('payment_type')['payment_value'].agg(['sum', 'count'])
@@ -28,20 +27,16 @@ def data_analysis(df):
     st.write("### Jumlah Transaksi berdasarkan Metode Pembayaran")
     st.bar_chart(payment_stats['count'])
 
-    # Menampilkan dataframe
-    st.write("## Dataframe")
-    st.dataframe(df)
+
 
 # Layout header
 def header():
     st.write("# Analisis Data E-Commerce")
-    st.write("Selamat datang di aplikasi analisis data e-commerce kami!")
-    st.write("Dengan aplikasi ini, Anda dapat menganalisis data transaksi e-commerce dan mendapatkan wawasan yang berharga.")
+    st.write("Selamat datang di aplikasi analisis data")
 
 # Layout footer
 def footer():
     st.write("Terima kasih telah menggunakan aplikasi ini. ")
-    st.write("Untuk sumber kode dan informasi lebih lanjut, kunjungi [github.com/username/repo](github.com/username/repo).")
 
 # Main function (recommended for clarity)
 def main():
